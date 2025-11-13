ad3d6b0553a0a6a1bee98fbae9b8d600c0ebc17f
Farnam Homayounfard <homfarnam@gmail.com>
2025-11-13 14:12:23 +0100

    feat: add data grid container component

---
+++ components/data-grid/container/container.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { DataGrid } from '@/components/data-grid/component';
+import { DataGridControls } from '@/components/data-grid/controls';
+import { FilterChips } from '@/components/filter-chips';
+import Loading from '@/components/shared/loading/loading';
+import { useDataGridState } from '@/hooks/use-data-grid-state';
+
+export const DataGridContainer = () => {
+  const {
+    searchQuery,
+    columns,
+    sorting,
+    pagination,
+    columnVisibility,
+    filters,
+    allOwners,
+    loading,
+    error,
+
+    processedDocuments,
+    totalDocuments,
+    totalPages,
+
+    handleToggleFavourite,
+    handleReset,
+    handleRemoveFilter,
+    handleSearchChange,
+    handleFiltersChange,
+    handlePageSizeChange,
+    handleColumnsChange,
+    handleSortingChange,
+    handlePaginationChange,
+  } = useDataGridState();
+
+  return (
+    <div className="space-y-4">
+      <DataGridControls
+        searchQuery={searchQuery}
+        onSearchChange={handleSearchChange}
+        columns={columns}
+        onColumnsChange={handleColumnsChange}
+        filters={filters}
+        onFiltersChange={handleFiltersChange}
+        allOwners={allOwners}
+        onReset={handleReset}
+      />
+
+      <FilterChips filters={filters} onRemoveFilter={handleRemoveFilter} />
+
+      {error && (
+        <div className="rounded-lg border border-red-200 bg-red-50 p-4 text-sm text-red-600">
+          <strong>Error:</strong> {error}
+        </div>
+      )}
+
+      <div className="relative">
+        {loading && <Loading />}
+
+        <DataGrid
+          documents={processedDocuments}
+          sorting={sorting}
+          onSortingChange={handleSortingChange}
+          pagination={pagination}
+          onPaginationChange={handlePaginationChange}
+          columnVisibility={columnVisibility}
+          onToggleFavourite={handleToggleFavourite}
+          totalDocuments={totalDocuments}
+          totalPages={totalPages}
+          onPageSizeChange={handlePageSizeChange}
+        />
+      </div>
+    </div>
+  );
+};
+
